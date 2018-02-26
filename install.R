aa0c872ce209454d8c148325b25fb0dc264acf94
Ben Marwick <benmarwick@gmail.com>
2018-02-26 00:26:16 -0800

    missing para in install.R

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
 install.packages(c( "tidyverse", "glue", "lubridate", "plotly", "purrrlyr",  "knitr", "sessioninfo", "git2r"))
-devtools::install_github(c("tidyverse/ggplot2", "PMassicotte/gtrendsR", "Ironholds/pageviews", "dgrtwo/fuzzyjoin")
+devtools::install_github(c("tidyverse/ggplot2", "PMassicotte/gtrendsR", "Ironholds/pageviews", "dgrtwo/fuzzyjoin"))
